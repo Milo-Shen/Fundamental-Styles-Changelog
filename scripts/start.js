deb2f7ddb314731afa10387c2ff0cd2088a1e874
Milo-Shen <Milo-Shen@outlook.com>
2023-11-10 21:53:36 +0800

    feat: update start

--- scripts/start.js
+++ scripts/start.js
@@ -14,20 +14,25 @@ const { execCommand, geMinVersion } = require("./util");
 // Paths
 const build_version = +new Date();
 const dirname = path.dirname(__dirname);
-const fundamental_repo = "https://github.com/SAP/ui5-webcomponents.git";
+const fundamental_repo = "https://github.com/SAP/fundamental-styles.git";
 const fundamental_folder = path.resolve(dirname, "fundamental-styles");
+const fiori_stories = path.resolve(fundamental_folder, "packages/styles/stories");
+console.log(fiori_stories);
+const work_folder = path.resolve(dirname, "work_folder");
 
 // Constant
 const EOL = os.EOL;
 const encoding = "utf8";
 
-const gitClean = () => {
+const gitToTag = (tag) => {
   process.chdir(fundamental_folder);
+  simpleGit().checkout(`v${tag}`);
   simpleGit().clean(simpleGit.CleanOptions.FORCE);
   process.chdir(dirname);
 };
 
 (async function () {
+  // git clone fundamental style repo
   const git = simpleGit();
   const isFundamentalAvailable = fs.existsSync(fundamental_folder);
 
@@ -36,16 +41,14 @@ const gitClean = () => {
     console.log(chalk.bold.green(`git clone ${fundamental_repo} successfully !`));
   }
 
-  gitClean();
-
+  // get released fundamental style lib versions from public NPM server
   let released_versions = await execCommand("npm view fundamental-styles versions --json");
   released_versions = JSON.parse(released_versions);
   released_versions = released_versions.filter((x) => !x.includes("rc"));
   released_versions = released_versions.filter(geMinVersion);
   console.log(released_versions);
 
-  // console.log(released_versions);
-  // released_versions = released_versions.filter((x) => !x.includes("rc"));
+  // download each version of fundamental-styles to work folder
 
-  console.log(1);
+  IO.mkFolderSyncRecursive(work_folder);
 })();
